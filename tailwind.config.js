44978c8af341e763b9a1c0f8da3b091c1c5aaf39
Bojo12344 <bojoishim@gmail.com>
2025-04-10 18:39:31 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-// Placeholder content for tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
