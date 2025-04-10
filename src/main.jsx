4dc91e4162d041ca4f454f040a911c434678de1d
Bojo12344 <bojoishim@gmail.com>
2025-04-10 18:47:56 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const App = () => {
-  return <h1>Hello from Simplify Dashboard!</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
